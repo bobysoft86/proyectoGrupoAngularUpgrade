1b68e1daa1919c6db40457fed27d3a2771272f69
Robert Rodriguez Carton <robertrodriguezcarton@gmail.com>
2024-01-22 21:14:37 +0100

    fix interface

--- src/app/interface/libros-interface.ts
+++ src/app/interface/libros-interface.ts
@@ -1,17 +1,8 @@
 export interface LibrosInterface {
-<<<<<<< HEAD
     id:number;
     img:string;
     title:string;
     autor:string;
     year:number;
     genre:string;
-=======
-  id: number;
-  img: string;
-  title: string;
-  autor: string;
-  year: number;
-  genre: string;
->>>>>>> 51fbfa2 (boton creado y linkeado)
 }
