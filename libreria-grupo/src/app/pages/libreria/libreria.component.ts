b991c697129cba0e20d16210502340b5e35e4235
Alba <albadvg@yahoo.es>
2024-01-22 15:08:16 +0100

    first commit

---
+++ libreria-grupo/src/app/pages/libreria/libreria.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-libreria',
+  standalone: true,
+  imports: [],
+  templateUrl: './libreria.component.html',
+  styleUrl: './libreria.component.scss'
+})
+export class LibreriaComponent {
+
+}
