6264dad8e804d575a1924a4664c626de0d3781d4
OhMyCris <cristina.crespo@bootcamp-upgrade.com>
2024-01-22 19:10:49 +0100

    cambio edit create

--- src/app/pages/create/create.component.ts
+++ src/app/pages/create/create.component.ts
@@ -1,12 +1,31 @@
+import { LibrosInterface } from './../../interface/libros-interface';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+
+  libro!:FormGroup;
+
+  constructor(private formBuilder:FormBuilder) {
+    this.libro = formBuilder.group({
+      img: ["", [Validators.required]],
+      title: ["", [Validators.required]],
+      autor: ["", [Validators.required]],
+      year: [0, [Validators.required]],
+      genre: ["", [Validators.required]]
+    })
+  }
+  
+  getLibro() {
+    console.log(this.libro.value)
+  }
+
 
 }
--- src/app/pages/edit/edit.component.ts
+++ src/app/pages/edit/edit.component.ts
@@ -1,32 +1,15 @@
-import { LibrosInterface } from './../../interface/libros-interface';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
 export class EditComponent {
   
-  libro!:FormGroup;
-
-  constructor(private formBuilder:FormBuilder) {
-    this.libro = formBuilder.group({
-      img: ["", [Validators.required]],
-      title: ["", [Validators.required]],
-      autor: ["", [Validators.required]],
-      year: [0, [Validators.required]],
-      genre: ["", [Validators.required]]
-    })
-  }
-  
-  getLibro() {
-    console.log(this.libro.value)
-  }
-
+ 
 
 
 }
