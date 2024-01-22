9e243424c24399542912c40427247928102d1825
CarolinaAlbanS <carolina.alban.sandoval@gmail.com>
2024-01-22 18:51:59 +0100

    libros pintados

--- src/app/pages/libreria/libreria.component.ts
+++ src/app/pages/libreria/libreria.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { GaleriaComponent } from '../../components/galeria/galeria.component';
 
 @Component({
   selector: 'app-libreria',
   standalone: true,
-  imports: [],
+  imports: [GaleriaComponent, RouterLink],
   templateUrl: './libreria.component.html',
   styleUrl: './libreria.component.scss',
 })
