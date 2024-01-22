fa5ef0138b054cebda80a2504a571533fa5f4838
CarolinaAlbanS <carolina.alban.sandoval@gmail.com>
2024-01-22 21:05:46 +0100

    funcion borrar

--- src/app/pages/book/book.component.ts
+++ src/app/pages/book/book.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { LibreriaComponent } from '../libreria/libreria.component';
+import { ApiService } from '../../services/api.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './book.component.html',
-  styleUrl: './book.component.scss'
+  styleUrl: './book.component.scss',
 })
 export class BookComponent {
+  id!: number;
+  producto!: any;
 
+  constructor(
+    private servicio: ApiService,
+    private rutaActiva: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    this.rutaActiva.paramMap.subscribe((params) => {
+      this.id = Number(params.get('id'));
+    });
+    this.servicio.getProductsid(this.id).subscribe((data: any) => {
+      this.producto = data;
+    });
+  }
+  deleteProducto() {
+    this.servicio.deleteProduct(this.id).subscribe();
+    alert('Producto eliminado');
+  }
 }
