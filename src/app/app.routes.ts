b054dc5e15db9a610604271d1a7396d8491dc528
CarolinaAlbanS <carolina.alban.sandoval@gmail.com>
2024-01-22 17:05:28 +0100

    routes y footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { LibreriaComponent } from './pages/libreria/libreria.component';
+import { BookComponent } from './pages/book/book.component';
+import { EditComponent } from './pages/edit/edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'libreria',
+    component: LibreriaComponent,
+  },
+  {
+    path: 'book/:id',
+    component: BookComponent,
+  },
+  {
+    path: 'book/:id/edit',
+    component: EditComponent,
+  },
+];
--- src/app/pages/libreria/libreria.component.ts
+++ src/app/pages/libreria/libreria.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-libreria',
   standalone: true,
   imports: [],
   templateUrl: './libreria.component.html',
-  styleUrl: './libreria.component.scss'
+  styleUrl: './libreria.component.scss',
 })
-export class LibreriaComponent {
-
-}
+export class LibreriaComponent {}
