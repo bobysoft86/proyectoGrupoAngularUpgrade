da7a3296dc504a0ef2896b22478586945835037b
Alba <albadvg@yahoo.es>
2024-01-22 23:23:17 +0100

    paste edit code

--- src/app/pages/edit/edit.component.ts
+++ src/app/pages/edit/edit.component.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { LibrosInterface } from '../../interface/libros-interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule,],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
 export class EditComponent {
-  
+ url:Array <any> = window.location.pathname.split('/');
+ libro!: LibrosInterface;
+
+  newLibro!:LibrosInterface;
+
+
+  constructor( private apiService: ApiService, private router: Router) {}
  
+
+  ngOnInit() {
+    this.apiService.getProductsid(this.url[3]).subscribe((res: any) => {
+      this.libro = res;
+      console.log('soy la lista', this.libro);
+    });
+  };
+
+  updateProduct(){
+    this.apiService.updateProduct(this.url[3],this.libro).subscribe()
+
+  console.log(this.libro);
+
+}
+
 
 
 }
